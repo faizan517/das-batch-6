2d33f8321fc043600dfdc1f9f828205d73a346a9
faizan517 <faizanpervaiz517@gmail.com>
2022-09-27 23:39:04 +0500

    assignment-5

---
+++ javascript/assignment-5/js/app.js
@@ -0,0 +1,48 @@
+//1 Prompt
+// prompt("what is your name","faizan")
+
+//2 parse prompt value in number
+// var numberOfCats = parseInt( prompt("How many cats?"));
+// var tooManyCats = numberOfCats + 1;
+// console.log("🚀 ~ file: app.js ~ line 6 ~ tooManyCats", tooManyCats)
+
+//3 comparison operators
+// > is greater than
+// < is less than
+// >= is greater than or equal to
+// <= is less than or equal to
+// !== is not equal to
+// ===  triple equal to check both value and number/string
+// == double equal to only check value
+
+//4 IF STATEMENT
+// if (1 > 0) {
+//   console.log(true);
+// }
+// if (1 >= 1) {
+//   console.log(true);
+// }
+// if (0 < 1) {
+//   console.log(true);
+// }
+// if (0 <= 0) {
+//   console.log(true);
+// }
+// if (0 !== 1) {
+//   console.log(true);
+// }
+// if (0 == "0") {
+//     console.log(true)
+// }
+// if (0 === 0) {
+//     console.log(true)
+// }
+
+//5 IF ELSE STATEMENT
+// if ("Rose" === "rose") {
+//    var a = console.log(true)
+// }else if(a === true){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
