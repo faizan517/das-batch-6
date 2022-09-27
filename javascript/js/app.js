57d57579dc75a188d626b724fd01f4256bdeec84
faizan517 <faizanpervaiz517@gmail.com>
2022-09-27 20:40:00 +0500

    assignment-4

---
+++ javascript/js/app.js
@@ -0,0 +1,30 @@
+//1 alert
+// alert("hello")
+
+//2 variables of string
+// var a = "string"
+
+//3 variables of number
+// var b = 123
+
+//4 familiar operators
+// var n1 = 2+2
+// var n2 = n1+1
+// var result = n1+n2
+
+//unfamiliar operators
+// num++ post increment
+// ++num pre increment
+// num-- post decrement
+// --num pre decrement
+// var n1 = 2+2
+// var n2 = ++n1
+// var result = n1+n2
+
+//math expressions
+// var math = 1-1*1+1%1/1
+// var n1 = 2*2
+// var n2 = 2+2
+// var n3 = 2-2
+// var n4 = 2/2
+// var n5 = 2%2
