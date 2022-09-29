06df1e89ebd8d6c1e1d0ff476ac5a5b62887cedc
faizan517 <faizanpervaiz517@gmail.com>
2022-09-29 22:01:58 +0500

    assignment-10

---
+++ javascript/assignment-10/js/app.js
@@ -0,0 +1,60 @@
+//1
+// var d = new Date()
+// console.log(d)
+
+//2
+// var enterMonth = prompt("Enter month number")
+// var month ='';
+// if (enterMonth == "1") {
+//     month = "jan"
+// }else if (enterMonth == '2') {
+//     month = "feb"
+// }else if (enterMonth == '3') {
+//     month = "mar"
+// }else if (enterMonth == '4') {
+//     month = "apr"
+// }else if (enterMonth == '5') {
+//     month = "may"
+// }else if (enterMonth == '6') {
+//     month = "jun"
+// }else if (enterMonth == '7') {
+//     month = "jul"
+// }else if (enterMonth == '8') {
+//     month = "aug"
+// }else if (enterMonth == '9') {
+//     month = "sep"
+// }else if (enterMonth == '10') {
+//     month = "oct"
+// }else if (enterMonth == '11') {
+//     month = "nov"
+// }else if (enterMonth == '12') {
+//     month = "dec"
+// }
+//     console.log( month)
+
+//3
+// var test = prompt("it's before noon or after noon?")
+// var answer = '';
+// if (test == 'before') {
+//     answer = 'AM'
+// }else if (test == 'after') {
+//     answer = 'PM'
+// }else{
+//     answer = "Enter before or after"
+// }
+// console.log(answer)
+
+//4 
+
+
+
+
+
+
+
+//5
+// var age = prompt("Enter your age")
+// var d = new Date()
+// var year = d.getFullYear()
+// var result = year - age
+// console.log(result)
