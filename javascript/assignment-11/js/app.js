8fd2836f6a080642bc66097b17677fc1ae3d0c0a
faizan517 <faizanpervaiz517@gmail.com>
2022-09-30 21:00:54 +0500

    assignment-11

---
+++ javascript/assignment-11/js/app.js
@@ -0,0 +1,43 @@
+// var math = 1.2323
+//round ---------------if value id 1.5 round the value upword if lessthan .5 round return downword value
+// var m = Math.round(math)
+// console.log(m)
+
+//ceil ---------------ceil always return upword round value
+// var m = Math.ceil(math)
+// console.log(m)
+
+//floor ---------------floor always return downword round value 
+// var m = Math.floor(math)
+// console.log(m)
+
+//random ---------------random return random value. return leass than 1 values
+// var m = Math.random()
+// console.log(m)
+
+//parseInt -------------- parseInt convert string into number and also cutoff the decimals value 
+// var myInteger = parseInt("1.9999");
+// console.log( myInteger)
+
+
+//parseFloat -------------- parseFloat convert string into number and not cutoff the decimals value 
+// var myInteger = parseFloat("1.9999");
+// console.log( myInteger)
+
+//Number -------------- Number work like parseFloat
+// var myInteger = Number("1.9999");
+// console.log( myInteger)
+
+//toFixed --------------- toFixed control decimal value
+// var fixes = math.toFixed(2) 
+// console.log(fixes)
+
+// toUppercase ------------- toUppercase as name says it's make string capital.
+// var nam = "faizan"
+// var n = nam.toUpperCase()
+// console.log(n)
+
+// toLocaleLowerCase ------------- toLocaleLowerCase as name says it's make string small.
+// var nam = "FAIZAN"
+// var n = nam.toLocaleLowerCase()
+// console.log(n)
