a32810039e2cb643b7d68e3e6b29ffa9f79fb4b5
faizan517 <faizanpervaiz517@gmail.com>
2022-10-05 02:48:23 +0500

    assignment-17

---
+++ javascript/assignment-17/js/app.js
@@ -0,0 +1,27 @@
+// var firstI = Number(prompt("Enter first number"));
+// var secondI = Number(prompt("Enter second number"));
+// var op = prompt("Enter Operator");
+// function calculator (firstI, secondI, op ) {
+//   switch (op) {
+//     case "+":
+//       console.log(firstI + secondI);
+//       break;
+//     case "-":
+//       console.log(firstI - secondI);
+//       break;
+//     case "*":
+//       console.log(firstI * secondI);
+//       break;
+//     case "/":
+//       console.log(firstI / secondI);
+//       break;
+//     case "%":
+//       console.log(firstI % secondI);
+//       break;
+
+//     default:
+//         console.log("enter number")
+//       break;
+//   }
+// }
+// calculator (firstI,secondI,op)
