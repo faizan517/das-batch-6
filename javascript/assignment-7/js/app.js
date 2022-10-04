cb105a76b59616fbd0e0a6d929133c419227ac43
faizan517 <faizanpervaiz517@gmail.com>
2022-10-05 02:12:52 +0500

    assignment-7

---
+++ javascript/assignment-7/js/app.js
@@ -0,0 +1,49 @@
+//Q1
+// var students = ['Ali','Faizan','Sami','Saad'];
+// var obtainedMarks = [100,200,300,400];
+// var totalMarks = 500;
+
+// console.log("Score of "+students[0]+" is "+obtainedMarks[0] +". The percentage is "+(obtainedMarks[0]/totalMarks)*100+"%" +". The grade is Fail")
+// console.log("Score of "+students[1]+" is "+obtainedMarks[1] +". The percentage is "+(obtainedMarks[1]/totalMarks)*100+"%" +". The grade is D")
+// console.log("Score of "+students[2]+" is "+obtainedMarks[2] +". The percentage is "+(obtainedMarks[2]/totalMarks)*100+"%" +". The grade is B")
+// console.log("Score of "+students[3]+" is "+obtainedMarks[3] +". The percentage is "+(obtainedMarks[3]/totalMarks)*100+"%" +". The grade is A-One")
+
+//Q2
+// var color =[]
+// var addcolorB = prompt("What color you want to add at the beginning of the array.").toString()
+// color.unshift(addcolorB)
+// var addcolorE = prompt("What color you want to add at the end of the array.").toString()
+// color.push(addcolorE)
+
+// var addcolorB = prompt("What color you want to add at the beginning of the array.").toString()
+// color.unshift(addcolorB)
+// var addcolorB = prompt("What color you want to add at the beginning of the array.").toString()
+// color.unshift(addcolorB)
+
+// var addcolorE = prompt("What color you want to add at the end of the array.").toString()
+// color.push(addcolorE)
+
+// var addcolorE = prompt("What color you want to add at the end of the array.").toString()
+// color.push(addcolorE)
+
+// color.shift()
+// color.pop()
+
+// var addcolorE = Number( prompt("At what index you want to add color "))
+// var acolor = prompt("Color name").toString()
+// color.splice(addcolorE,0,acolor)
+
+// var addcolorE = Number( prompt("strating index "))
+// var acolor = prompt("how many?").toString()
+// color.splice(addcolorE,acolor)
+// console.log( color)
+
+//Q3
+// var enterNum = prompt("Enter Number")
+// var num = enterNum;
+// if (num) {
+//     for (let i = 1; i <= 10; i++) {
+
+//         console.log(num + ' * ' + i + " = " + num*i)
+//     }
+// }
