a70235d83fa4aba3710ed5afb6c69905a07c8af4
faizan517 <faizanpervaiz517@gmail.com>
2022-10-11 02:01:03 +0500

    Assignment-21

--- javascript/assignment-21/js/app.js
+++ javascript/assignment-21/js/app.js
@@ -1,39 +1,55 @@
-// setTimeout(function() {
-//         console.log('Executed')
-//     },500)
+var min = 0;
+var sec = 0;
+var mili = 0;
+var disMin = 0;
+var disMili = 0;
+var dispSec = 0;
+var check = "stop";
 
-//    var time= setInterval(function() {
-//             console.log('Hello')
-//         },500)
-//  var t
-// function start() {
-//   t= setInterval(function() {
-//         console.log("www")
-//         },500) 
-//         console.log(t)
-//         return t
-// }
-
-//     function stop() {
-//         clearTimeout(t)    
-//     }
-    
-//     function clear() {
-//         clearInterval(t)
-       
-        
-//     }
-
-var timeoutId=null;
-var check = "stop"
-
-function start(){
-    if(check==="stop"){
-        timeoutId=window.setInterval(timer,1000);
-        document.getElementById("start").innerHTML="Stop"; 
-        check="start";
-    }else{
-    window.clearInterval(timeoutId);
-    document.getElementById("start").innerHTML="Start";
-    check="stop";
-}}
+function start() {
+  if (check === "stop") {
+    timeout = setInterval(timer, 17);
+    document.getElementById("start").innerHTML = "Stop";
+    check = "start";
+  } else {
+    clearInterval(timeout);
+    document.getElementById("start").innerHTML = "Start";
+    check = "stop";
+  }
+}
+function timer() {
+  sec++;
+  if (sec / 60 == 1) {
+    mili++;
+    sec = 0;
+    if (mili / 60 == 1) {
+      min++;
+      mili = 0;
+    }
+  }
+  if (sec < 10) {
+    dispSec = "0" + sec.toString();
+  } else {
+    dispSec = sec.toString();
+  }
+  if (mili < 10) {
+    disMili = "0" + mili.toString();
+  } else {
+    disMili = mili.toString();
+  }
+  if (min < 10) {
+    disMin = "0" + min.toString();
+  } else {
+    disMin = min.toString();
+  }
+  document.getElementById("timer").innerHTML =
+    disMin + ":" + disMili + ":" + dispSec;
+}
+function reset() {
+  clearInterval(timeout);
+  sec = 0;
+  mili = 0;
+  min = 0;
+  document.getElementById("timer").innerHTML = "00:00:00";
+  document.getElementById("start").innerHTML = "Start";
+}
