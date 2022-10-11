3418851002dc1e12357745b2e05c5e366f2d2386
faizan517 <faizanpervaiz517@gmail.com>
2022-10-12 01:42:00 +0500

    assignment-18

--- javascript/assignment-18/js/app.js
+++ javascript/assignment-18/js/app.js
@@ -1,9 +1,25 @@
-// var input = document.getElementById('id').innerText 
-// var select = document.getElementById('select').innerText
-// var btn = document.getElementById('update')
-// console.log(select)
-// console.log(input)
-
-// for (let i = 0; i < select.length; i++) {
-//   console.log(select[i])
-// }
+function submit() {
+  var select = document.getElementById("select").value;
+  var input = document.getElementById("class").value;
+  if (select == 1) {
+    document.getElementById("1").innerText = input;
+  } else if (select == 2) {
+    document.getElementById("2").innerText = input;
+  } else if (select == 3) {
+    document.getElementById("3").innerText = input;
+  } else if (select == 4) {
+    document.getElementById("4").innerText = input;
+  } else if (select == 5) {
+    document.getElementById("5").innerText = input;
+  } else if (select == 6) {
+    document.getElementById("6").innerText = input;
+  } else if (select == 7) {
+    document.getElementById("7").innerText = input;
+  } else if (select == 8) {
+    document.getElementById("8").innerText = input;
+  } else if (select == 9) {
+    document.getElementById("9").innerText = input;
+  } else if (select == 10) {
+    document.getElementById("10").innerText = input;
+  }
+}
